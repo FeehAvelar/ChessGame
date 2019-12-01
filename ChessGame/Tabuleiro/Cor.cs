9cfc683ecd77d4428ff4d379105874e077968ba7
Felipe Avelar <felipealpaca@gmail.com>
2019-12-01 16:08:21 -0300

    Comecando a implementar Tabuleiro

---
+++ ChessGame/Tabuleiro/Cor.cs
@@ -0,0 +1,14 @@
+﻿namespace Tabuleiro
+{
+    enum Cor
+    {
+        White = 0,
+        Black = 1,
+        Green = 2,
+        Red = 3,
+        Yellow = 4,
+        Blue = 5,
+        Orange = 6
+
+    }
+}
---
+++ ChessGame/Tabuleiro/Peca.cs
@@ -0,0 +1,23 @@
+﻿using Tabuleiro;
+
+namespace Tabuleiro
+{
+    abstract class Peca
+    {
+        public Posicao Position { get; set; }
+        public Cor Color { get; protected set; }
+        public int AmountMoviment { get; set; }
+        public Tabuleiro Tab { get; protected set; }
+
+        public Peca (Posicao position, Tabuleiro tab, Cor color)
+        {
+            Position = position;
+            Tab = tab;
+            Color = color;
+            AmountMoviment = 0;
+        }
+
+        public abstract void Mover(Posicao newPosition);
+
+    }
+}
