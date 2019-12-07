7ca2ec6011a9842ef6656a009b88ec52d7747199
Felipe Avelar <felipealpaca@gmail.com>
2019-12-07 09:45:55 -0300

    Implementando restricoes de movimento

--- ChessGame/Entities/JogoXadrez/PartidaXadrez.cs
+++ ChessGame/Entities/JogoXadrez/PartidaXadrez.cs
@@ -1,4 +1,5 @@
 ﻿using Tabuleiro;
+using ChessGame.Entities.Exceptions;
 
 namespace JogoXadrez
 {
@@ -6,13 +7,13 @@ namespace JogoXadrez
     {
         public GameBoard Tab { get; private set; }
         public int Turno { get; private set; }
-        public Cor jogadorAtual { get; private set; }
+        public Cor JogadorAtual { get; private set; }
         public bool Terminada { get; private set;  }
         public PartidaXadrez()
         {
             Tab = new GameBoard(8, 8);
             Turno = 1;
-            jogadorAtual = Cor.White;
+            JogadorAtual = Cor.White;
             Terminada = false;
             ColocarPecas();
         }
@@ -23,6 +24,47 @@ namespace JogoXadrez
             p.IncrementMoviment();
             Peca pecaCapturada = Tab.retirarPeca(destino);
             Tab.colocarPeca(p, destino);            
+        }
+
+        public void ValidarOrigem(Posicao pos)
+        {
+            if (Tab.PegaPeca(pos) == null)
+                throw new GameBoardException("Não existe peça no local selecionado");
+
+            if (Tab.PegaPeca(pos).Color != JogadorAtual)
+                throw new GameBoardException("A peça de origem é não é sua");
+
+            if (!Tab.PegaPeca(pos).ExisteMovimentosPossiveis())
+                throw new GameBoardException("Não há movimentos possíveis para a peça de origem escolhida");
+        }
+
+        public void ValidaDestino (Posicao origem,Posicao destino)
+        {
+            if (origem == destino)
+                throw new GameBoardException("A peça tem que ser movida");
+            if (!Tab.PegaPeca(origem).PodeMoverPara(destino))
+            {
+                throw new GameBoardException("Posição de destino inválida");
+            }
+        }
+
+        public void RealizaJogada (Posicao origem, Posicao destino)
+        {
+            ExecutaMovimento(origem, destino);
+            Turno++;
+            MudaJogador();
+        }
+
+        private void MudaJogador()
+        {
+            if (JogadorAtual == Cor.White)
+            {
+                JogadorAtual = Cor.Black;
+            }
+            else
+            {
+                JogadorAtual = Cor.White;
+            }
         }
 
         private void ColocarPecas()
--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -19,9 +19,12 @@ namespace ChessGame
                     Console.Clear();
                     Console.WriteLine();
                     Tela.ImprimirTabuleiro(partida.Tab);
+                    Console.WriteLine("\nTurno {0}",partida.Turno);
+                    Console.WriteLine($"Aguardando jogado: {partida.JogadorAtual}");
 
                     Console.Write("\nDigtie a origem: ");
                     Posicao origem = Tela.LerPosicaoXadrez().ConvertPosition();
+                    partida.ValidarOrigem(origem);
 
                     Console.Clear();
                     bool[,] posicoesPossiveis = partida.Tab.PegaPeca(origem).MovimentosPossiveis();
@@ -31,13 +34,15 @@ namespace ChessGame
 
                     Console.Write("\nDestino: ");
                     Posicao destino = Tela.LerPosicaoXadrez().ConvertPosition();
+                    partida.ValidaDestino(origem, destino);
 
-                    partida.ExecutaMovimento(origem, destino);
+                    partida.RealizaJogada(origem, destino);
                 }
             }
             catch (GameBoardException e)
             {
                 Console.WriteLine("Chess game error: " + e.Message);
+                Console.ReadLine();
             }
             catch (Exception e)
             {
